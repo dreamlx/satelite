56e2d66d74ef221104cbfc6e2ef5b3cd19873a18
jasonly <jason.hua@ripple-tech.com>
2017-09-25 22:39:16 +0800

    fix ab_path

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,20 @@
 class PhotosController < ApplicationController
   def index
     @photo = Photo.new
+    # require 'open3'
+    # require 'pathname'
+    # root_path = Pathname.new(__FILE__)
+    # root_path = root_path.to_s.gsub('/app/controllers/photos_controller.rb', '')
+    # images_path = "#{root_path}/public/test_images/beach1.jpeg"
+    #
+    # cmd = "python #{root_path}/lib/assets/satelite/retrain_model_classifier.py #{root_path}_,_#{images_path}"
+    # Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thread|
+    #   @aa = {err: stderr.read.chomp,
+    #          out: stdout.read.chomp
+    #   }
+    #   stdin.close
+    # end
+
   end
 
   def create
@@ -12,15 +26,17 @@ class PhotosController < ApplicationController
         redirect_to photos_path
       else
         flash[:notice] = '上传成功'
-        @image_paths = ['../../../public'+image.name_url]
-        ensure_file_exist = true
-        if ensure_file_exist
-          @result = `python lib/assets/satelite/retrain_model_classifier.py #{@image_paths.join(',')}`
-          redirect_to "#{photos_path}?result=#{@result}"
-        else
-          flash[:notice] = '500 error'
-          redirect_to photos_path
+        require 'open3'
+        require 'pathname'
+        root_path = Pathname.new(__FILE__)
+        root_path = root_path.to_s.gsub('/app/controllers/photos_controller.rb', '')
+        images_path = "#{root_path}/public#{image.name_url}"
+        cmd = "python #{root_path}/lib/assets/satelite/retrain_model_classifier.py #{root_path}_,_#{images_path}"
+        Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thread|
+          @result = {err: stderr.read.chomp, out: stdout.read.chomp}
+          stdin.close
         end
+        redirect_to "#{photos_path}?result=#{@result}&image_path=#{images_path}&root_path=#{root_path}"
       end
     else
       flash[:error] = '请上传一张图片'
--- lib/assets/satelite/retrain_model_classifier.py
+++ lib/assets/satelite/retrain_model_classifier.py
@@ -2,19 +2,20 @@
 import sys
 import os
 import tensorflow as tf
-from PIL import Image
+#from PIL import Image
 
 # image_paths = sys.argv[1]
 
-# Read in the image_data
-# image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+params = sys.argv[1].split('_,_')
+ab_path = params[0]
+images_path = [params[1]]
 
 # Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line
-               in tf.gfile.GFile("./output_dir/satellite_labels.txt")]
+               in tf.gfile.GFile(ab_path + "/lib/assets/satelite/output_dir/satellite_labels.txt")]
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("./output_dir/satellite_graph.pb", 'rb') as f:
+with tf.gfile.FastGFile(ab_path + "/lib/assets/satelite/output_dir/satellite_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
@@ -25,7 +26,7 @@ with tf.Session() as sess:
 
     result = {}
     # 遍历目录
-    for image_path in sys.argv[1].split(','):
+    for image_path in images_path:
         # for index, image_path in enumerate(['images/test_images/beach.jpg', 'images/test_images/beach1.jpeg']):
         result[image_path] = []
         image_data = tf.gfile.FastGFile(image_path, 'rb').read()
@@ -35,5 +36,5 @@ with tf.Session() as sess:
             human_string = label_lines[node_id]
             score = predictions[0][node_id]
             result[image_path].append({"type": human_string, "score": score})
-            #print('%s (score = %.5f)' % (human_string, score))
+            # print('%s (score = %.5f)' % (human_string, score))
     print(result)
